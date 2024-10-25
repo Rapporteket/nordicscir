888a87bafb10f391d12af03c962620e3f40e0b84
arnfinn <arnfinn.steindal@gmail.com>
2024-10-25 10:31:34 +0200

    Riktig log-key

--- doc/oppsett.R
+++ doc/oppsett.R
@@ -5,6 +5,6 @@ devtools::install(upgrade = FALSE)
 Sys.setenv(FALK_EXTENDED_USER_RIGHTS="[{\"A\":80,\"R\":\"LC\",\"U\":1},{\"A\":80,\"R\":\"SC\",\"U\":2},{\"A\":81,\"R\":\"LC\",\"U\":2}]")
 Sys.setenv(R_RAP_INSTANCE="QAC")
 Sys.setenv(R_RAP_CONFIG_PATH="/home/rstudio/nordicscir/data-raw/config")
-
+Sys.unsetenv("MYSQL_PORT_LOG")
 
 nordicscir::kjor_NSapper(register='nordicscir')
